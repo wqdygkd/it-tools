8c223bfb257a66eef7780ee1960f55abd255509f
wqdygkd <cui3155@gmail.com>
2024-07-22 01:34:16 +0800

    更新

--- src/tools/formatter/json-formatter/json-formater.vue
+++ src/tools/formatter/json-formatter/json-formater.vue
@@ -2,3 +2,25 @@ json格式化
 
 // 对象文本
 // json文本
+<script>
+const a = '{a: 1}'
+// const a = {a: 1}
+// const a ={"a": 1}
+const b = '"{a: 1}"'
+const c = '{"a": 1}'
+const d = '"{\\"a\\": 1}"'
+
+var obj = eval('(' + a + ')')
+// e = '"{a":1'
+// {a:1
+
+b: 2
+{a: 1
+
+先判断是
+if (否为 花括号包裹) {
+  eval()
+} else {
+
+}
+</script>
